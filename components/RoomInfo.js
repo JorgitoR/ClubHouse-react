8d94636d222434a44acd5da4d922dbdc8ccc044e
JorgitoR <jorgitouribe133@gmail.com>
2021-06-03 10:27:56 -0500

    Clubhouse-clone React

--- components/RoomInfo.js
+++ components/RoomInfo.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 import style from "../style/roomCard.module.css";
 import data from "../Data/roomCard.json";
 
+import {ChatDots, BsChatDotsFill, BsFillPersonFill} from 'react-icons/bs';
+
 export default function RoomInfoCard(props){
 
 	return (
@@ -15,6 +17,21 @@ export default function RoomInfoCard(props){
 						<div className={style.roomCardContainer}>
 							<h6>{item.title}</h6>
 							<h2>{item.sub_title}</h2>
+							<div className={style.roomMembers}>
+								<div>
+									<img src="/images/user-img.jpg" alt="" />
+									<img src="/images/user-img2.jpg" alt="" />
+								</div>
+								<div>
+									{item.members.map((person) => (
+
+										<p>
+										 {person.first_name} {person.last_name} 
+										</p>
+
+									))}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
