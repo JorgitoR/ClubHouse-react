cf56d9f69d5c68b7a391c26252a71e2a057ac1db
JorgitoR <jorgitouribe133@gmail.com>
2021-06-03 11:27:54 -0500

    Clubhouse-clone React

---
+++ components/BottomSheet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SwipeableBottomSheet from "react-swipeable-bottom-sheet";
+import style from "../style/bottom.mudule.css";
+
+
+
+export default function BottomSheet(props){
+
+	return (
+
+
+	)
+
+}
