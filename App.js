c6e1174f81b00c665462cbc3934ea102e57ec0ed
JorgitoR <jorgitouribe133@gmail.com>
2021-06-03 06:16:00 -0500

    ClubHouse-clone react

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import PlanLayout from './pages/layouts/planLayout';
 import { BrowserRouter, Route } from 'react-router-dom';
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import CodeConfirm from './pages/CodeConfirm';
+import notification from './pages/notification';
 //npm install react-router
 //npm install react-router-dom
 //npm install react-phone-number-input
@@ -16,7 +17,8 @@ function App(){
   		<PlanLayout>
   			<Route exact path="/" component={Bienvenido} />
   			<Route exact path="/invite" component={PhoneConfirmation}  />	
-  			<Route exact path="/code_confirm" component={CodeConfirm}  />	
+  			<Route exact path="/code_confirm" component={CodeConfirm}  />
+  			<Route exact path="/notification" component={notification}  />		
   		</PlanLayout>
 
   	</BrowserRouter>
