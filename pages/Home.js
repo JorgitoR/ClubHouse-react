900205637a35bfccec29356f6c81c5f21504d284
JorgitoR <jorgitouribe133@gmail.com>
2021-06-03 08:31:48 -0500

    Clubhouse-clone React

--- pages/Home.js
+++ pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/header";
 import DayliInfoCard from "../components/DailyInfoCard";
+import RoomInfo from "../components/RoomInfo";
 import style from "../style/home.module.css";
 
 export default function Home(){
@@ -12,6 +13,7 @@ export default function Home(){
 
 		<div className={style.home_container}>
 			<DayliInfoCard />
+			<RoomInfo />
 		</div>
 		</>
 	);	
