e27f8c3da8cc915bfbfdc69b788f2402f3989912
JorgitoR <jorgitouribe133@gmail.com>
2021-06-03 09:10:00 -0500

    Clubhouse-clone React

--- components/RoomInfo.js
+++ components/RoomInfo.js
@@ -1,1 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import style from "../style/roomCard.module.css";
+import data from "../Data/roomCard.json";
+
+export default function RoomInfoCard(props){
+
+	return (
+
+		<>
+			<h2></h2>
+			
+
+		</>
+
+	)
+
+
+}
